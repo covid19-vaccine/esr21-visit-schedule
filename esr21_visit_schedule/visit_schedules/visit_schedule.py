e1c1e9b5137fb8525b1452822f93b8dfb2d85c43
tumiemosweu <imosweuk@gmail.com>
2021-05-25 18:13:01 +0200

    update visit schedule

--- esr21_visit_schedule/visit_schedules/visit_schedule.py
+++ esr21_visit_schedule/visit_schedules/visit_schedule.py
@@ -4,10 +4,10 @@ from .schedule import esr21_enrollment_schedule, esr21_fu_schedule
 
 esr21_visit_schedule = VisitSchedule(
     name='esr21_visit_schedule',
-    verbose_name='ESR@!',
-    offstudy_model='esr21.subjectlocator',
+    verbose_name='ESR21',
+    offstudy_model='esr21_subject.subjectlocator',
     locator_model='esr21_subject.subjectlocator',
-    death_report_model='',
+    death_report_model='esr21_subject.subjectlocator',
     previous_visit_schedule=None)
 
 esr21_visit_schedule.add_schedule(esr21_enrollment_schedule)
